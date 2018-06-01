46c2bc1280c2d84b8c5b7a57e599c567149fd385
MacStudent <macstudent@20X-iMacXX-M-448.local>
2018-06-01 15:03:20 -0400

    employee class added

--- src/employeemanagement/EmployeeManagement.java
+++ src/employeemanagement/EmployeeManagement.java
@@ -15,7 +15,12 @@ public class EmployeeManagement {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        employee e1 = new employee();
+        e1.employeeId = 1;
+        e1.employeeName = "mann";
+        e1.salary = 12220;
+        e1.display();
     }
     
 }
---
+++ src/employeemanagement/employee.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeemanagement;
+
+/**
+ *
+ * @author macstudent
+ */
+public class employee {
+    int employeeId;
+    String employeeName;
+    float salary;
+    public void setValues(int id,String name,float sal)
+    {
+        employeeId = id;
+        employeeName = name;
+        salary = sal;
+    }
+    public void display()
+    {
+        System.out.println(employeeId);
+        System.out.println(employeeName);
+        System.out.println(salary);
+    }
+   
+}
